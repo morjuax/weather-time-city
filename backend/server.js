d3b428a5af8ee9b37ac6d2ec7180a56d8767878d
Juan Moreno <morjua19@gmail.com>
2019-09-27 19:28:54 -0300

    [FIX]

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
 
